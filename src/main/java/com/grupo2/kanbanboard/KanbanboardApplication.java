63effe25de73eb15e993968cd5af6c8d4bd61952
Jaranag <joel.aranag@gmail.com>
2024-07-08 09:51:42 +0200

    initial commit

---
+++ src/main/java/com/grupo2/kanbanboard/KanbanboardApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo2/kanbanboard/KanbanboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
