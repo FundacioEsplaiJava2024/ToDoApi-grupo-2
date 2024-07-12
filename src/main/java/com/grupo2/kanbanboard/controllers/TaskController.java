2e5c75be96ccd5ca4672afa088888e536acfb8dc
Jaranag <joel.aranag@gmail.com>
2024-07-12 13:30:26 +0200

    added change status function

--- src/main/java/com/grupo2/kanbanboard/controllers/TaskController.java
+++ src/main/java/com/grupo2/kanbanboard/controllers/TaskController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.grupo2.kanbanboard.entities.Task;
 import com.grupo2.kanbanboard.entities.TaskStatusEnum;
 import com.grupo2.kanbanboard.requests.CreateTaskInput;
+import com.grupo2.kanbanboard.requests.UpdateTaskInput;
 import com.grupo2.kanbanboard.services.TaskService;
+
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,11 +28,28 @@ public class TaskController {
 
     @PostMapping("/tasks")
     public ResponseEntity<?> createTasks(@RequestBody CreateTaskInput createTaskInput) {
-        if (createTaskInput.status() != "toDoTasks" || createTaskInput.status() != "doingTasks" || createTaskInput.status() != "doneTasks" ) {
+        if (createTaskInput.status() != "toDoTasks" || createTaskInput.status() != "doingTasks"
+                || createTaskInput.status() != "doneTasks") {
             return new ResponseEntity<>("Input a valid status.", HttpStatus.NOT_FOUND);
         }
-        Task taskCreated = taskService.create(createTaskInput.name(), createTaskInput.projectId(), TaskStatusEnum.valueOf(createTaskInput.status()));
+        Task taskCreated = taskService.create(createTaskInput.name(), createTaskInput.projectId(),
+                TaskStatusEnum.valueOf(createTaskInput.status()));
         return new ResponseEntity<>(taskCreated, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("tasks/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable int id,
+            @RequestBody UpdateTaskInput updateTaskInput) {
+        Optional<Task> optionalTask = taskService.findById(id);
+
+        if (optionalTask.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Task taskToUpdate = optionalTask.get();
+
+        Task taskUpdated = taskService.changeStatus(updateTaskInput.status(), taskToUpdate);
+        return new ResponseEntity<>(taskUpdated, HttpStatus.OK);
     }
 
     @DeleteMapping("/tasks/{id}")
