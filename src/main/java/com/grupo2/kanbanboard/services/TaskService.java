e02969ccd0f18f7eba700be5623af4e5d57ff8a1
Alfredo <josealfredomiguelcarrillo@gmail.com>
2024-07-12 11:23:01 +0200

    Create a Task Conadded create function to projecttroller

---
+++ src/main/java/com/grupo2/kanbanboard/services/TaskService.java
@@ -0,0 +1,34 @@
+package com.grupo2.kanbanboard.services;
+
+import org.apache.el.stream.Optional;
+import org.springframework.stereotype.Service;
+
+import com.grupo2.kanbanboard.entities.Project;
+import com.grupo2.kanbanboard.entities.Task;
+import com.grupo2.kanbanboard.entities.TaskStatusEnum;
+import com.grupo2.kanbanboard.repositories.ProjectRepository;
+import com.grupo2.kanbanboard.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
+
+
+    public TaskService (TaskRepository taskRepository, ProjectRepository projectRepository) {
+        this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
+
+    }
+    
+    public Task create (String TaskName, int idProject, TaskStatusEnum taskStatus) {
+
+        Task task = new Task();
+        task.setName(TaskName);
+        task.setStatus(taskStatus);
+        Optional<Project> optionalProject = projectRepository.findById(idProject);  
+        Project taskProject = optionalProject.get();
+        task.setProject(taskProject);
+
+    }
+}
