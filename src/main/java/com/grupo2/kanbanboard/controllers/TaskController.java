566a25729c0fe0fb2547210c655389c619d76d00
Jaranag <joel.aranag@gmail.com>
2024-07-12 13:22:12 +0200

    delete endpoint

--- src/main/java/com/grupo2/kanbanboard/controllers/TaskController.java
+++ src/main/java/com/grupo2/kanbanboard/controllers/TaskController.java
@@ -9,6 +9,8 @@ import com.grupo2.kanbanboard.services.TaskService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -27,6 +29,12 @@ public class TaskController {
         }
         Task taskCreated = taskService.create(createTaskInput.name(), createTaskInput.projectId(), TaskStatusEnum.valueOf(createTaskInput.status()));
         return new ResponseEntity<>(taskCreated, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable int id) {
+        taskService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
