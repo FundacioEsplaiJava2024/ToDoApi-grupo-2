fc8aa2a311008837ed36b312b64e37fd6022acae
Jaranag <joel.aranag@gmail.com>
2024-07-24 10:55:18 +0200

    created user and project relationship

--- src/main/java/com/grupo2/kanbanboard/entities/User.java
+++ src/main/java/com/grupo2/kanbanboard/entities/User.java
@@ -1,14 +1,18 @@
 package com.grupo2.kanbanboard.entities;
 
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
-@Table(name="users")
+@Table(name = "users")
 @Entity
 public class User {
     @Id
@@ -21,6 +25,14 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<Project> projects;
+
+    public void dismissProject(Project project) {
+        this.projects.remove(project);
+        project.setUser(null);
+    }
 
     public void setId(Integer id) {
         this.id = id;
