462adaae17ba79ea7bda1f044da5617ea057acbf
Jaranag <joel.aranag@gmail.com>
2024-07-24 13:10:04 +0200

    Created security filter

---
+++ src/main/java/com/grupo2/kanbanboard/auth/SecurityFilter.java
@@ -0,0 +1,44 @@
+package com.grupo2.kanbanboard.auth;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.grupo2.kanbanboard.repositories.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+  @Autowired
+  TokenProvider tokenService;
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    var token = this.recoverToken(request);
+    if (token != null) {
+      var login = tokenService.validateToken(token);
+      var user = userRepository.findByLogin(login);
+      var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+    filterChain.doFilter(request, response);
+  }
+
+  private String recoverToken(HttpServletRequest request) {
+    var authHeader = request.getHeader("Authorization");
+    if (authHeader == null)
+      return null;
+    return authHeader.replace("Bearer ", "");
+  }
+}
