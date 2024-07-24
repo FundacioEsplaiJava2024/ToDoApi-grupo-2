6c3f2cfb11890c7fd6e18bd73a1416d618fa59e6
Jaranag <joel.aranag@gmail.com>
2024-07-24 13:56:45 +0200

    Created auth service and added dependency to check tokens

---
+++ src/main/java/com/grupo2/kanbanboard/services/AuthService.java
@@ -0,0 +1,37 @@
+package com.grupo2.kanbanboard.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.grupo2.kanbanboard.entities.User;
+import com.grupo2.kanbanboard.repositories.UserRepository;
+import com.grupo2.kanbanboard.requests.LoginInput;
+
+import net.eulerframework.common.util.jwt.InvalidJwtException;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+  @Autowired
+  UserRepository repository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) {
+    var user = repository.findByLogin(username);
+    return user;
+  }
+
+  public UserDetails signUp(LoginInput data) throws InvalidJwtException {
+    if (repository.findByLogin(data.login()) != null) {
+      throw new InvalidJwtException("Username already exists");
+    }
+    String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+    User newUser = new User();
+    newUser.setLogin(data.login());
+    newUser.setPassword(encryptedPassword);
+    return repository.save(newUser);
+  }
+}
