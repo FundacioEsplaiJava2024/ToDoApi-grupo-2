b124186229af46ef3dd8af47ca82ec1bf7bad94f
Jaranag <joel.aranag@gmail.com>
2024-07-24 12:49:29 +0200

    user repo implementing jpa repository and implementing findbylogin

--- src/main/java/com/grupo2/kanbanboard/repositories/UserRepository.java
+++ src/main/java/com/grupo2/kanbanboard/repositories/UserRepository.java
@@ -1,11 +1,10 @@
 package com.grupo2.kanbanboard.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.grupo2.kanbanboard.entities.User;
 
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-    
-}
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
+  }
