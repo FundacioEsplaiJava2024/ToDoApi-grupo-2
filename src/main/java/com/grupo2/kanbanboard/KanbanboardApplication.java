7dfbdc97976c64a6da43d852feb907851082427f
Jaranag <joel.aranag@gmail.com>
2024-07-26 10:16:14 +0200

    web secret hardcoded for now

--- src/main/java/com/grupo2/kanbanboard/KanbanboardApplication.java
+++ src/main/java/com/grupo2/kanbanboard/KanbanboardApplication.java
@@ -2,10 +2,8 @@ package com.grupo2.kanbanboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:${user.dir}/.env")
 public class KanbanboardApplication {
 
 	public static void main(String[] args) {
